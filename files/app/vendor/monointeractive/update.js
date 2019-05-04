191af301aa10dd8b4401b730fe18eedc3fe5c591
monointeractive <44318178+monointeractive@users.noreply.github.com>
2019-05-04 23:45:18 +0200

    Update update.js

--- files/app/vendor/monointeractive/update.js
+++ files/app/vendor/monointeractive/update.js
@@ -12,5 +12,10 @@ update.events.on('start',function(){
 });
 
 update.events.on('reject',function(data){
+	if(fs.existsSync(path.join(execDir,'updates','mbs.exe'))){
+		try{
+			fs.copyFileSync(path.join(execDir,'updates','mbs.exe'), path.join(execDir,'mbs.exe'));				
+		} catch(err){console.error(err);}
+	}
 	tray.changeIcon(!data.reject.isError ? 'default' : 'warning');	
 });
